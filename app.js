d75c1a59e17cea6301b4de82b332b21567ae496b
MosNes <7hekarl@gmail.com>
2022-08-25 16:24:03 -0400

    started #6 Event Modifiers

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 new Vue({
-    el: ''
+    el: '#vue-app',
+    data: {
+        name: 'MosNes',
+        job: 'Bomp',
+        website: 'https://github.com/Mosnes',
+        websiteTag: '<a href="https://github.com/Mosnes">GitHub from v-html</a>',
+        age: 34,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        greet: function(timeOfDay){
+            this.name
+            return `Good ${timeOfDay}, ${this.name}`
+        },
+        addAge: function(num){
+            this.age+=num;
+        },
+        subtractAge: function(num){
+            this.age-=num;
+        },
+        updateXY: function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        }
+    }
 });
